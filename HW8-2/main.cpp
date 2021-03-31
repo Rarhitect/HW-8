99b0e960368814af8a82c744474c0577b42dc3cf
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-03-31 16:56:39 +0300

    Реализовал последовательный алгоритм! (настоящее счастье)

--- HW8-2/main.cpp
+++ HW8-2/main.cpp
@@ -47,6 +47,49 @@ int main(int argc, const char * argv[])
     std::cout << "Enter the quantity of DNA elements: ";
     int quantity = 0;
     std::cin >> quantity;
-    std::cout << "Here is your random DNA: " << random_dna(quantity) << std::endl;
+    std::string dna = random_dna(quantity);
+    std::cout << "Here is your random DNA: " << dna << std::endl;
+    
+    std::string user_search;
+    std::cout << "Insert the part you searching: ";
+    std::cin >> user_search;
+    
+    std::string dna_copy = dna;
+    std::vector< std::size_t > indexes;
+
+    std::size_t new_index = 0;
+  
+    std::string::iterator it = dna.begin();
+    while(it <= dna.end())
+    {
+        static int iterration_number = 0;
+        std::size_t positions = dna_copy.find(user_search);
+
+        if (positions == std::string::npos)
+        {
+            break;
+        }
+        new_index += positions;
+        indexes.push_back(new_index + 1 + 2*iterration_number);
+        std::advance(it, (positions + user_search.size()));
+        dna_copy = dna_copy.substr(positions + user_search.size());
+        ++iterration_number;
+    }
+    
+    if(indexes.empty())
+    {
+        std::cout << "No such elements" << std::endl;
+        exit(1);
+    }
+    else
+    {
+        for(auto & element: indexes)
+        {
+            std::cout << element << " ";
+        }
+    }
+    
+    std::cout << std::endl;
+    
     return 0;
 }
