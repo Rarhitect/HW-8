d973332c63145d38a8b3096723b1677aefd6eea6
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-03-31 13:11:04 +0300

    Реализовали генерацию случайного ДНК

--- HW8-2/main.cpp
+++ HW8-2/main.cpp
@@ -6,9 +6,47 @@
 //
 
 #include <iostream>
+#include <string>
+#include <thread>
+#include <random>
+#include <vector>
+
+std::string random_dna(int quantity)
+{
+    std::string dna;
+    std::random_device rd;
+    std::mt19937 mersenne(rd());
+    std::uniform_int_distribution< int > uni(1, 4);
+    
+    for(auto i = 0; i < quantity; ++i)
+    {
+        int code = uni(mersenne);
+        if (code == 1)
+        {
+            dna += 'A';
+        }
+        else if (code == 2)
+        {
+            dna += 'G';
+        }
+        else if (code == 3)
+        {
+            dna+= 'T';
+        }
+        else if (code == 4)
+        {
+            dna += 'C';
+        }
+    }
+    
+    return dna;
+}
 
 int main(int argc, const char * argv[])
 {
-    
+    std::cout << "Enter the quantity of DNA elements: ";
+    int quantity = 0;
+    std::cin >> quantity;
+    std::cout << "Here is your random DNA: " << random_dna(quantity) << std::endl;
     return 0;
 }
