4611c7a63ebb9beec5de0c8b4f081e110092560a
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-04-01 14:41:44 +0300

    Полировка

--- HW8-3/main.cpp
+++ HW8-3/main.cpp
@@ -10,22 +10,22 @@
 #include <mutex>
 #include <queue>
 
-template < typename T >
-class Threadsafe_Queue
+template <typename T>
+class Threadsafe_Priority_Queue
 {
 public:
 
-    Threadsafe_Queue() = default;
+    Threadsafe_Priority_Queue() = default;
 
-    Threadsafe_Queue(const Threadsafe_Queue & other)
+    Threadsafe_Priority_Queue(const Threadsafe_Priority_Queue& other)
     {
         std::lock_guard < std::mutex > lock(other.m_mutex);
         m_queue = other.m_queue;
     }
 
-    Threadsafe_Queue & operator=(const Threadsafe_Queue & other)
+    Threadsafe_Priority_Queue& operator=(const Threadsafe_Priority_Queue& other)
     {
-        std::scoped_lock lock(m_mutex, other.m_mutex);
+        std::lock_guard < std::mutex > lock(other.m_mutex);
         m_queue = other.m_queue;
     }
 
@@ -38,30 +38,30 @@ public:
         m_condition_variable.notify_one();
     }
 
-    void wait_and_pop(T & value)
+    void wait_and_pop(T& value)
     {
         std::unique_lock < std::mutex > lock(m_mutex);
-        
+
         m_condition_variable.wait(lock, [this] {return !m_queue.empty(); });
-        value = m_queue.front();
+        value = m_queue.top();
         m_queue.pop();
     }
 
-    std::shared_ptr < T > wait_and_pop()
+    std::shared_ptr <T> wait_and_pop()
     {
         std::unique_lock < std::mutex > lock(m_mutex);
-        
+
         m_condition_variable.wait(lock, [this] {return !m_queue.empty(); });
-        auto result = std::make_shared < T > (m_queue.top());
+        auto result = std::make_shared < T >(m_queue.top());
         m_queue.pop();
-        
+
         return result;
     }
 
-    bool try_pop(T & value)
+    bool try_pop(T& value)
     {
         std::lock_guard < std::mutex > lock(m_mutex);
-        
+
         if (m_queue.empty())
         {
             return false;
@@ -76,15 +76,15 @@ public:
     std::shared_ptr < T > try_pop()
     {
         std::lock_guard < std::mutex > lock(m_mutex);
-        
+
         if (m_queue.empty())
         {
-            return std::shared_ptr < T > ();
+            return std::shared_ptr < T >();
         }
-            
-        auto result = std::make_shared < T > (m_queue.top());
+
+        auto result = std::make_shared < T >(m_queue.top());
         m_queue.pop();
-        
+
         return result;
     }
 
@@ -95,18 +95,14 @@ public:
     }
 
 private:
-
-    std::priority_queue < T >        m_queue;
+    std::priority_queue <T> m_queue;
     std::condition_variable m_condition_variable;
-
-private:
-
     mutable std::mutex m_mutex;
 };
 
-int main(int argc, char ** argv)
+int main()
 {
-    Threadsafe_Queue < int > queue;
+    Threadsafe_Priority_Queue <int> queue;
 
     queue.push(42);
 
